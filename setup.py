251d426bcabc758bd99f5671eea66f1e3dbff9e5
Guilherme Barros <guilhermebarrosap@gmail.com>
2021-11-18 22:27:53 -0300

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Projeto relativo ao curso de mlops deploy da Alura',
+    author='guilherme.barros',
+    license='',
+)
---
+++ src/app/main.py
@@ -0,0 +1,39 @@
+from flask import Flask, request, jsonify
+from flask_basicauth import BasicAuth
+from textblob import TextBlob
+import pickle
+from sklearn.linear_model import LinearRegression
+import os
+
+with open('../../models/modelo_preco', 'rb') as f:
+    modelo = pickle.load(f)
+
+colunas = ['tamanho', 'ano', 'garagem']
+
+app = Flask(__name__)
+app.config['BASIC_AUTH_USERNAME'] = os.environ.get('BASIC_AUTH_USERNAME')
+app.config['BASIC_AUTH_PASSWORD'] = os.environ.get('BASIC_AUTH_PASSWORD')
+
+basicauth = BasicAuth(app)
+
+@app.route('/')
+def home():
+    return 'Minha primeira API'
+
+@app.route('/sentimento/<frase>')
+@basicauth.required
+def sentimento(frase):
+    tb = TextBlob(frase)
+    tb_en = tb.translate(to='en')
+    polaridade = tb_en.sentiment.polarity
+    return f'Polaridade: {polaridade}'
+
+@app.route('/cotacao/', methods=['POST'])
+@basicauth.required
+def cotacao():
+    dados = request.get_json()
+    dados_input = [dados[col] for col in colunas]
+    preco = modelo.predict([dados_input])
+    return jsonify(preco=preco[0])
+
+app.run(debug=True, host='0.0.0.0')
